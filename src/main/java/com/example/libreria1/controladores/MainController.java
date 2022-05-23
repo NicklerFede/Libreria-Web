91b35f78960ce73da22e6b24735b0119d71d3ccd
Federico Nickler <fedenickler@gmail>
2022-05-22 22:38:50 -0300

    Cambios en frontend

--- src/main/java/com/example/libreria1/controladores/MainController.java
+++ src/main/java/com/example/libreria1/controladores/MainController.java
@@ -1,5 +1,6 @@
 package com.example.libreria1.controladores;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/inicio")
 public class MainController {
-    
+
     @GetMapping("")
-    public String index(){
-	return"index";
+    public String index(Principal principal) {
+        if (principal == null) {
+            return "redirect:/";
+        }
+        return "index";
     }
 }
-	
